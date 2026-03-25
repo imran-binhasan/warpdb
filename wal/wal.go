f8ac49fb7a14eab8bec3d4217de0c456a929ce9d
imran-binhasan <imranbinhasan.work@gmail.com>
2026-03-25 19:51:43 +0600

    add write, sync, replay funcs for WAL(write ahead log)

--- wal/wal.go
+++ wal/wal.go
@@ -1,13 +1,79 @@
 package wal
 
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"os"
-// 	"sync"
-// )
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"sync"
+)
 
 type Entry struct {
-	Op string `json:"op"`
+	Op   string   `json:"op"`
 	Args []string `json:"args"`
+}
+
+type Wal struct {
+	file    *os.File
+	encoder *json.Encoder
+	mu      sync.Mutex
+}
+
+type Replayer interface {
+	Set(key, value string) error
+	Del(key string) error
+}
+
+func NewWAL(path string) (*Wal, error) {
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+	encoder := json.NewEncoder(file)
+	return &Wal{
+		file:    file,
+		encoder: encoder,
+	}, nil
+}
+
+func (w *Wal) Write(op string, args []string) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	entry := Entry{op, args}
+	return w.encoder.Encode(entry)
+}
+
+func (w *Wal) Sync() error {
+	return w.file.Sync()
+}
+
+func (w *Wal) Replay(engine Replayer) error {
+	_, err := w.file.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+	decoder := json.NewDecoder(w.file)
+	for {
+		var entry Entry
+		err := decoder.Decode(&entry)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		switch entry.Op {
+		case "SET":
+			if len(entry.Args) != 2 {
+				continue
+			}
+			engine.Set(entry.Args[0], entry.Args[2])
+		case "DEL":
+			if len(entry.Args) != 1 {
+				continue
+			}
+			engine.Del(entry.Args[0])
+		}
+	}
+	return nil
 }
